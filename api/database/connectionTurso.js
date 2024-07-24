38564849b389108a6ae322f9b939a8633710a53d
gustipardo <gusm2003@gmail.com>
2024-07-24 16:41:23 -0300

    Only turso

--- api/database/connectionTurso.js
+++ api/database/connectionTurso.js
@@ -3,7 +3,7 @@ import { createClient } from "@libsql/client";
 
 config();
 
-export const turso = createClient({
+export const db = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
--- api/models/local-file-system/invoices.js
+++ api/models/local-file-system/invoices.js
@@ -1,17 +1,16 @@
 import { config } from "dotenv";
 // Aca tiene que ser SQLite3 Local o  puede ser tanto local como production?
-import {db} from "../../database/connection.js";
-import { turso } from "../../database/connectionTurso.js";
-
+// El metodo de db de sqlite3 local no es el mismo que el de Turso, por el momento solo uso turso
+/* import {db} from "../../database/connection.js"; */
+import { db } from "../../database/connectionTurso.js";
+ 
 config();
 
 export class invoicesModel {
   static async getAllInvoices() {
     try {
-/*       const invoices = await db.allAsync("SELECT * FROM invoices"); */
-          const invoices = await turso.execute("SELECT * FROM users");
-
-          console.log("data", invoices.rows);
+      const invoices = await db.execute("SELECT * FROM invoices");
+      console.log("data", invoices.rows);
 
       return invoices;
     } catch (err) {
