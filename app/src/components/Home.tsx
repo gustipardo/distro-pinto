c04bd5e1c4fc534789e3e28690a2393a2c962ca7
gustipardo <gusm2003@gmail.com>
2024-07-26 15:52:08 -0300

    build fix and charts added to home

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./Navbar.css";
 import { Button } from "@/components/ui/button"
 import {Chart} from "./Chart";
+import { Chart2 } from "./Chart2";
 
 const Home: React.FC = () => {
   return (
     <div>Home
         <Button>Click me</Button>
+        <Chart2 />
+
         <Chart />
     </div>
     
