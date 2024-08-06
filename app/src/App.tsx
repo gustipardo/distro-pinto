2071b1976a49d107631838aeb6e493a4b8a79a1d
gustipardo <gusm2003@gmail.com>
2024-08-06 16:07:09 -0300

    Chart added again

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,7 @@ import {Sidebar} from "./components/Sidebar";
 import InvoicesList from "./components/pages/InvoicesList";
 import {AddInvoice} from "./components/pages/AddInvoice";
 import {Placeholder} from "./components/Placeholder";
+import Home from "./components/Home";
 import { Suppliers } from "./components/pages/Suppliers";
 
 export const App = () => {
@@ -14,7 +15,7 @@ export const App = () => {
         <Route path="/facturas" element={<InvoicesList />} />
         <Route path="/agregar-facturas" element={<AddInvoice />} />
         <Route path="/placeholder" element={<Placeholder />} />
-        <Route path="/" element={<Suppliers />} />
+        <Route path="/" element={<Home />} />
         <Route path="/proveedores" element={<Suppliers />} />
       </Routes>
     </div>
