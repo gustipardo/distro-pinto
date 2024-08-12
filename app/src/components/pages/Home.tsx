06b059af96a8b8fe4e6a6423ef597975345f0761
gustipardo <gusm2003@gmail.com>
2024-08-11 22:48:29 -0300

    Calendar Range Picker added from johnpolacek

--- app/src/components/pages/Home.tsx
+++ app/src/components/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { DateRangePicker } from "../reusable/date-range-picker";
 
 const Home: React.FC = () => {
   return (
     <div>Home
+      <DateRangePicker />ftrg54
     </div>
     
   );
