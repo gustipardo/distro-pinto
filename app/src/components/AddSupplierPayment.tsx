e092d90fb0bc795dd98d0b60ffb85f0fd53409f6
gustipardo <gusm2003@gmail.com>
2024-09-17 09:08:57 -0300

    form reset added

--- app/src/components/AddSupplierPayment.tsx
+++ app/src/components/AddSupplierPayment.tsx
@@ -23,8 +23,8 @@ import { addPayment } from "@/services/addPayment";
 import { PaymentMethod } from "@/commons/Interfaces";
 
 interface AddSupplierPayment {
-    onPaymentAdded: () => void;
-    invoice_id: number;
+  onPaymentAdded: () => void;
+  invoice_id: number;
 }
 
 export const AddSupplierPayment = ({ onPaymentAdded, invoice_id }: AddSupplierPayment) => {
@@ -47,14 +47,14 @@ export const AddSupplierPayment = ({ onPaymentAdded, invoice_id }: AddSupplierPa
   }, [form]);
 
   const onSubmit = async (values: { date: string; amount: number; payment_method: PaymentMethod }) => {
-    console.log("Formulario enviado con valores:", values, invoice_id, "expense" );
-    const result = await addPayment({invoice_id, date: values.date, amount: values.amount, payment_method: values.payment_method, type: "expense"});
+    console.log("Formulario enviado con valores:", values, invoice_id, "expense");
+    const result = await addPayment({ invoice_id, date: values.date, amount: values.amount, payment_method: values.payment_method, type: "expense" });
     console.log("Respuesta de la API:", result);
     setOpen(false);
-    console.log()
+    form.reset()
     if (result.success) {
       console.log("Se agregó la factura con éxito");
-      onPaymentAdded(); 
+      onPaymentAdded();
     }
   };
 
@@ -71,57 +71,57 @@ export const AddSupplierPayment = ({ onPaymentAdded, invoice_id }: AddSupplierPa
   return (
 
     <Popover open={open} onOpenChange={setOpen}>
-    <PopoverTrigger asChild>
-        <Button><PlusIcon className="mr-2 h-4 w-4"/>Ingresar Pago</Button>
+      <PopoverTrigger asChild>
+        <Button><PlusIcon className="mr-2 h-4 w-4" />Ingresar Pago</Button>
       </PopoverTrigger>
-    <PopoverContent>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-        <FormField
-          control={form.control}
-          name="date"
-          render={() => (
-            <FormItem>
-              <FormLabel>Fecha</FormLabel>
-              <FormControl>
-                <CalendarPicker onDateChange={handleDateChange} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <PopoverContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="date"
+              render={() => (
+                <FormItem>
+                  <FormLabel>Fecha</FormLabel>
+                  <FormControl>
+                    <CalendarPicker onDateChange={handleDateChange} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <FormField
-          control={form.control}
-          name="amount"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Total</FormLabel>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              <FormMessage />
-          </FormItem>
-          )}
-        />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Total</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <FormField
-          control={form.control}
-          name="payment_method"
-          render={() => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Tipo de Pago</FormLabel>
-              <FormControl>
-                <SelectTypeOfPayment onPaymentChange={handlePaymentChange} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="w-full" type="submit">Agregar</Button>
-      </form>
-    </Form>
-    </PopoverContent>
+            <FormField
+              control={form.control}
+              name="payment_method"
+              render={() => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Tipo de Pago</FormLabel>
+                  <FormControl>
+                    <SelectTypeOfPayment onPaymentChange={handlePaymentChange} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit">Agregar</Button>
+          </form>
+        </Form>
+      </PopoverContent>
     </Popover>
 
 
